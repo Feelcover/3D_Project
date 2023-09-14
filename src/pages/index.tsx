3a5517c7294de116070f4ac5c8f3d3eaa0267129
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-14 15:56:48 +0300

    создать Home.tsx подключить тайлвинд

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Home from "@/components/screens/home/Home";
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return (
-    <div>
-    <h1>Portfolio</h1>
-    </div>
-  )
+export default function HomePage() {
+  return <Home />;
 }
