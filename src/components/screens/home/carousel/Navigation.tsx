4b1562a04c6c0f0a79c992be706a825c9cb451dd
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-17 23:15:49 +0300

    сделать функционал для Navigation

--- src/components/screens/home/carousel/Navigation.tsx
+++ src/components/screens/home/carousel/Navigation.tsx
@@ -1,9 +1,19 @@
+import useActions from "@/hooks/useActions";
 import React, { FC } from "react";
+import { BsChevronDoubleRight, BsChevronDoubleLeft } from "react-icons/bs";
 
 const Navigation: FC = () => {
-  return <div>
-    
-  </div>;
+  const { nextSlide, prevSlide } = useActions();
+  return (
+    <div>
+      <button onClick={() => prevSlide()}>
+        <BsChevronDoubleRight />
+      </button>
+      <button onClick={() => nextSlide()}>
+        <BsChevronDoubleLeft />
+      </button>
+    </div>
+  );
 };
 
 export default Navigation;
