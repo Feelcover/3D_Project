bd393b16c1e770cf47a20e93057e6edad94a1ed6
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-20 10:50:49 +0300

    развенуть компонент Three для тестирования

---
+++ src/components/screens/home/carousel/Three.tsx
@@ -0,0 +1,25 @@
+import { useFrame } from "@react-three/fiber";
+import React, { FC, useRef, useState } from "react";
+
+const Three: FC = () => {
+  const ref = useRef();
+  const [hovered, hover] = useState(false);
+  const [clicked, click] = useState(false);
+  useFrame((state, delta) => (ref.current.rotation.x += delta));
+
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+export default Three;
