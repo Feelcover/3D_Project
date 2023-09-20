8ef94bf4ccb74ad0fce074c5a4e99047f315240f
Andrey Chebanov <pioneerbeat@yandex.ru>
2023-09-20 10:55:15 +0300

    рефакторинг и создание компонента Box

--- src/components/screens/home/carousel/Three.tsx
+++ src/components/screens/home/carousel/Three.tsx
@@ -1,25 +1,17 @@
-import { useFrame } from "@react-three/fiber";
-import React, { FC, useRef, useState } from "react";
+import { Canvas } from '@react-three/fiber'
+import React from 'react'
+import Box from './Box'
 
-const Three: FC = () => {
-  const ref = useRef();
-  const [hovered, hover] = useState(false);
-  const [clicked, click] = useState(false);
-  useFrame((state, delta) => (ref.current.rotation.x += delta));
 
+const Three = () => {
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => hover(true)}
-      onPointerOut={(event) => hover(false)}
-    >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
-    </mesh>
-  );
-};
+    <Canvas>
+    <ambientLight />
+    <pointLight position={[10, 10, 10]} />
+    <Box position={[-1.2, 0, 0]} />
+    <Box position={[1.2, 0, 0]} />
+  </Canvas>
+  )
+}
 
-export default Three;
+export default Three
